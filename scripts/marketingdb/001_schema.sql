38b2a842cff74e9e77a86ff0b5b50b8d871b8229
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-07-11 15:07:59 +0400

    add data for da second project

--- scripts/marketingdb/001_schema.sql
+++ scripts/marketingdb/001_schema.sql
@@ -1,50 +1,51 @@
-DROP TABLE IF EXISTS user_leads_payments;
-CREATE TABLE user_leads_payments (
-	visitor_id VARCHAR(255) PRIMARY KEY,
-	amount NUMERIC,
-	created_at VARCHAR(255),
-	closing_reason VARCHAR(255),
-	learning_format VARCHAR(255),
-	status_id BIGINT
+DROP TABLE IF EXISTS leads;
+CREATE TABLE leads(
+   visitor_id      VARCHAR(255)
+  ,amount          INTEGER
+  ,created_at      TIMESTAMP
+  ,closing_reason  VARCHAR(255)
+  ,learning_format VARCHAR(255)
+  ,status_id       BIGINT
 );
 
-DROP TABLE IF EXISTS user_sessions;
-CREATE TABLE user_sessions (
-	visitor_id VARCHAR(255),
-	datetime VARCHAR(255),
-	landing_page VARCHAR(255),
-	source VARCHAR(255),
-	medium VARCHAR(255),
-	campaign VARCHAR(255),
-	content VARCHAR(255),
-	term VARCHAR(255)
+
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions(
+   visitor_id   VARCHAR(255)
+  ,datetime     TIMESTAMP
+  ,landing_page VARCHAR(700)
+  ,source       VARCHAR(255)
+  ,medium       VARCHAR(255)
+  ,campaign     VARCHAR(255)
+  ,content      VARCHAR(255)
 );
+
 
 DROP TABLE IF EXISTS vk_ads;
-CREATE TABLE vk_ads (
-	id VARCHAR(255),
-	ad_name VARCHAR(255),
-	campaign_id BIGINT,
-	campaign_name VARCHAR(255),
-	date VARCHAR(255),
-	spent NUMERIC,
-	utm_source VARCHAR(255),
-	utm_medium VARCHAR(255),
-	utm_campaign VARCHAR(255),
-	utm_content VARCHAR(255),
-	utm_term VARCHAR(255)
+CREATE TABLE vk_ads(
+   id            VARCHAR(255)
+  ,ad_name       VARCHAR(255)
+  ,campaign_id   VARCHAR(255)
+  ,campaign_name VARCHAR(255)
+  ,date          TIMESTAMP
+  ,spent         INTEGER
+  ,utm_source    VARCHAR(255)
+  ,utm_medium    VARCHAR(255)
+  ,utm_campaign  VARCHAR(255)
+  ,utm_content   VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS yandex_ads;
-CREATE TABLE yandex_ads (
-	id VARCHAR(255),
-	campaign_id BIGINT,
-	campaign_name VARCHAR(255),
-	utm_source VARCHAR(255),
-	utm_medium VARCHAR(255),
-	utm_campaign VARCHAR(255),
-	utm_content VARCHAR(255),
-	utm_term VARCHAR(255),
-	date VARCHAR(255),
-	cost NUMERIC
+
+DROP TABLE IF EXISTS ya_ads;
+CREATE TABLE ya_ads(
+   id            VARCHAR(255)
+  ,ad_name       VARCHAR(255)
+  ,campaign_id   VARCHAR(255)
+  ,campaign_name VARCHAR(255)
+  ,date          TIMESTAMP
+  ,spent         INTEGER
+  ,utm_source    VARCHAR(255)
+  ,utm_medium    VARCHAR(255)
+  ,utm_campaign  VARCHAR(255)
+  ,utm_content   VARCHAR(255)
 );
